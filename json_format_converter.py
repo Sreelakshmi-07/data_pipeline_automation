321237de66550190af552e7647821558e2367c5b
sreelakshmi-07 <sreelakshmi0777@gmail.com>
2024-11-13 20:53:26 +0530

    script for converting json to excel/csv/xml files

--- json_format_converter.py
+++ json_format_converter.py
@@ -1,5 +1,7 @@
 import json
 import pandas as pd
+from json2xml import json2xml
+from json2xml.utils import readfromurl, readfromstring, readfromjson
 
 def flatten_dict(d, parent_key='', sep='_'):
 
@@ -67,17 +69,34 @@ def json_to_csv(input_file, output_file):
     df.to_csv(output_file, index=False)
     print(f"CSV file saved to {output_file}")
 
+def json_to_xml(input_file, output_file):
+    """Convert the JSON data from a file into an XML file."""
+
+    # Read the JSON file
+    with open(input_file, 'r') as file:
+        json_data = json.load(file)
+
+    # Convert JSON to XML string
+    xml_data = json2xml.Json2xml(json_data).to_xml()
+
+    # Save the XML string to a file
+    with open(output_file, 'w') as file:
+        file.write(xml_data)
+
+    print(f"XML file saved to {output_file}")
 
 # Example usage
 input_file = input("Enter path: ") # Path to your JSON file 
 output_path = f"{input_file.rsplit('/', 1)[-1]}"
 output_excel = output_path.replace(".json", ".xlsx") # Path to save the Excel file
 output_csv = output_path.replace(".json", ".csv")  # Path to save the CSV file
+output_xml = output_path.replace(".json", ".xml")  # Path to save the XML file
 
 # Convert JSON to Excel
 json_to_excel(input_file, output_excel)
 # Convert JSON to CSV
 json_to_csv(input_file, output_csv)
-
+# Convert JSON to XML
+json_to_xml(input_file, output_xml)
 
 
